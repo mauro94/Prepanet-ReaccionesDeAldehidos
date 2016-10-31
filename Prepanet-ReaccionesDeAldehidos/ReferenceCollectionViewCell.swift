1c89a59bf85a6481e0bdd50edc5ef7cbf18309e3
Mauro Amarante <mauro.amarante94@gmail.com>
2016-10-31 17:54:29 -0600

    initial xcode commit
    basic UI for reference section completed

---
+++ Prepanet-ReaccionesDeAldehidos/ReferenceCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ReferenceCollectionViewCell.swift
+//  Prepanet-ReaccionesDeAldehidos
+//
+//  Created by Mauro Amarante on 10/28/16.
+//  Copyright © 2016 Mauro Amarante. All rights reserved.
+//
+
+import UIKit
+
+class ReferenceCollectionViewCell: UICollectionViewCell {
+	@IBOutlet weak var lbNumber: UILabel!
+	@IBOutlet weak var lbCode: UILabel!
+	@IBOutlet weak var lbName: UILabel!
+}
---
+++ Prepanet-ReaccionesDeAldehidos/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Prepanet-ReaccionesDeAldehidos
+//
+//  Created by Mauro Amarante on 10/28/16.
+//  Copyright © 2016 Mauro Amarante. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
