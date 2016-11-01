2bf1dcc1f15a0d8c1674809e8bc0a5fb536a0a43
Mauro Amarante <mauro.amarante94@gmail.com>
2016-11-01 15:18:13 -0600

    realm added

---
+++ Prepanet-ReaccionesDeAldehidos/gameData.swift
@@ -0,0 +1,21 @@
+//
+//  gameData.swift
+//  Prepanet-ReaccionesDeAldehidos
+//
+//  Created by Mauro Amarante on 11/1/16.
+//  Copyright © 2016 Mauro Amarante. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class gameData: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+	
+	
+}
