1e2b01c8ccd0250c7c330136265d325a343e703c
Fabian <f.montemayor26@gmail.com>
2016-11-06 17:35:13 -0600

    Exam: questions/answers are shown

--- ExamViewController.swift
+++ ExamViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ExamViewController: UIViewController {
-
+    
+    @IBOutlet weak var btnAnswer2: UIButton!
+    @IBOutlet weak var btnAnswer4: UIButton!
+    @IBOutlet weak var btnAnswer3: UIButton!
+    @IBOutlet weak var btnAnswer1: UIButton!
+    
+    lazy var buttons: [UIButton] = { return [self.btnAnswer1, self.btnAnswer2, self.btnAnswer3, self.btnAnswer4] }()
+    
+    @IBOutlet weak var lblQuestionWImg: UILabel!
+    @IBOutlet weak var lblQuestionNoImg: UILabel!
+    @IBOutlet weak var imgQuestion: UIImageView!
+    
+    var difficulty = "Easy"
+    var points = 0
+    var questions = [Question]()
+    var currentQuestionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        points = 0
+        
+        let realm = try! Realm()
+        var exam = Exam()
+        if difficulty != "" {
+            let predicate = NSPredicate(format: "name == %@", difficulty)
+            exam = realm.objects(Exam.self).filter(predicate).first!
+        }
+        else {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        questions = exam.questions.shuffled()
+        
+        updateLabelsAndButtonsForIndex(questionIndex: 0)
+        
         // Do any additional setup after loading the view.
 		self.title = "Examen"
     }
@@ -22,7 +54,59 @@ class ExamViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func updateLabelsAndButtonsForIndex(questionIndex : Int) {
+        if questionIndex < questions.count - 1{
+            currentQuestionIndex = questionIndex
+            let currentQuestion = questions[questionIndex]
+            if currentQuestion.image != nil {
+                // Question has an image
+                
+                // Update question
+                lblQuestionNoImg.isHidden = true
+                lblQuestionWImg.isHidden = false
+                imgQuestion.isHidden = false
+                imgQuestion.image = UIImage(named: currentQuestion.image!)
+                lblQuestionWImg.text = currentQuestion.question  
+            }
+            else {
+                // Question has no image
+                // Update question
+                lblQuestionNoImg.isHidden = false
+                lblQuestionWImg.isHidden = true
+                imgQuestion.isHidden = true
+                lblQuestionNoImg.text = currentQuestion.question
+            }
+            
+            // Update buttons
+            var answers = deserializeString(value: currentQuestion.wrong, separator: "%")
+            answers.append(currentQuestion.right)
+            answers = answers.shuffled()
+            
+            for (answerIndex, button) in buttons.enumerated() {
+                button.setTitle(answers[answerIndex], for: .normal)
+                button.isHidden = false
+            }
+        }
+        else {
+            lblQuestionWImg.isHidden = true
+            imgQuestion.isHidden = true
+            lblQuestionNoImg.isHidden = false
+            lblQuestionNoImg.text = "¡Terminaste! Tu puntiación es: " + "\(points)" + "/7"
+            
+            for button in buttons {
+                button.isHidden = true
+            }
+        }
+    }
+    
+    
+    @IBAction func didTapNextButton(_ sender: UIButton) {
+        updateLabelsAndButtonsForIndex(questionIndex: currentQuestionIndex + 1)
+    }
+    
+    @IBAction func didTapEndButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
@@ -33,4 +117,28 @@ class ExamViewController: UIViewController {
     }
     */
 
+}
+
+extension MutableCollection where Indices.Iterator.Element == Index {
+    /// Shuffles the contents of this collection.
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (unshuffledCount, firstUnshuffled) in zip(stride(from: c, to: 1, by: -1), indices) {
+            let d: IndexDistance = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
+            guard d != 0 else { continue }
+            let i = index(firstUnshuffled, offsetBy: d)
+            swap(&self[firstUnshuffled], &self[i])
+        }
+    }
+}
+
+extension Sequence {
+    /// Returns an array with the contents of this sequence, shuffled.
+    func shuffled() -> [Iterator.Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
 }
