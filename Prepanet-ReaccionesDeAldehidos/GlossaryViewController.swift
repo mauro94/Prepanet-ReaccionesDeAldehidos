dcbcca1e2cab5a2f05a16494300278d63b09ae02
Mauro Amarante <mauro.amarante94@gmail.com>
2016-11-08 15:36:15 -0600

    assets warning and constraints fixes

--- Prepanet-ReaccionesDeAldehidos/GlossaryViewController.swift
+++ Prepanet-ReaccionesDeAldehidos/GlossaryViewController.swift
@@ -1,23 +1,42 @@
 //
-//  Reglas1ViewController.swift
+//  GlosarioViewController.swift
 //  Prepanet-ReaccionesDeAldehidos
 //
-//  Created by Mauro Amarante on 11/6/16.
+//  Created by Mauro Amarante on 11/7/16.
 //  Copyright © 2016 Mauro Amarante. All rights reserved.
 //
 
 import UIKit
 
-class Reglas1ViewController: UIViewController {
-	@IBOutlet weak var welcomeView: UIView!
-	
+class GlossaryViewController: UIViewController {
+	@IBOutlet weak var img1: UIImageView!
+	@IBOutlet weak var img2: UIImageView!
+	@IBOutlet weak var img3: UIImageView!
+	@IBOutlet weak var img4: UIImageView!
+	@IBOutlet weak var lb1: UILabel!
+	@IBOutlet weak var lb2: UILabel!
+	@IBOutlet weak var lb3: UILabel!
+	@IBOutlet weak var lb4: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-		//let backgroundImage = UIImageView(image: UIImage(named: "gameBackground"))
-		//backgroundImage.frame.size = welcomeView.frame.size
-		//welcomeView.addSubview(backgroundImage)
+		if (self.view.frame.size.height <= 640) {
+			img1.isHidden = false
+			img2.isHidden = false
+			img3.isHidden = false
+			img4.isHidden = false
+			lb1.isHidden = false
+			lb2.isHidden = false
+			lb3.isHidden = false
+			lb4.isHidden = false
+		}
+		
+		if (self.view.frame.size.height <= 750) {
+			img1.isHidden = false
+			lb1.isHidden = false
+		}
     }
 
     override func didReceiveMemoryWarning() {
